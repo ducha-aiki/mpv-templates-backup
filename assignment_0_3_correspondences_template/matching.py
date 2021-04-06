190af8f6a689c67286ebbf643942dd251a9449d9
Mgr. Dmytro Mishkin <ducha.aiki@gmail.com>
2021-04-06 14:47:04 +0200

    fixed docgtring

--- assignment_0_3_correspondences_template/matching.py
+++ assignment_0_3_correspondences_template/matching.py
@@ -40,8 +40,7 @@ def match_snn(desc1: torch.Tensor, desc2: torch.Tensor, th: float = 0.8):
     return matches_idxs, match_dists
 
 def match_mnn(desc1: torch.Tensor, desc2: torch.Tensor):
-    '''Function, which finds mutual nearest neightbors for each vector in desc1 and desc2,
-    which satisfies first to second nearest neighbor distance <= th check
+    '''Function, which finds mutual nearest neightbors for each vector in desc1 and desc2.
     
     Return:
         torch.Tensor: indexes of matching descriptors in desc1 and desc2
