2ee1aa736fb816c05f3e36181ba75dee809782df
Dmytro Mishkin <ducha.aiki@gmail.com>
2023-03-08 10:36:29 +0100

    added sift and orientation visualization

--- assignment_0_3_correspondences_template/oriviz.py
+++ assignment_0_3_correspondences_template/oriviz.py
@@ -19,6 +19,8 @@ from tkinter import ttk
 from ipywidgets import interact, interactive, fixed, interact_manual
 import mpl_interactions.ipyplot as iplt
 import time
+import matplotlib.image as mpimg
+import matplotlib.patches as mpatches
 from kornia.feature import get_laf_orientation, set_laf_orientation, extract_patches_from_pyramid
 
 FULL_ROTATION = 360
@@ -88,3 +90,90 @@ def img_viz(img: torch.tensor, A: torch.tensor, orientation_estimation, fig, ax1
   ax3.set_data(img3)
   display(fig)
   plt.close()
+
+
+""" The SIFT visualization """
+
+
+
+# Visualization 
+
+def reshape_to_bins(siftdesc, spatial_dim, orient_dim):
+  b, dim = siftdesc.shape
+  return siftdesc.reshape(b, orient_dim, -1).reshape(b, orient_dim, spatial_dim, spatial_dim)
+
+def visualize_sift(desc,  orient_dim=8, spatial_dim=4, title = '', is_absolute_mode =1):
+  res = 5*reshape_to_bins(desc, spatial_dim, orient_dim)
+  max = -1.0
+  angle = 360.0 / orient_dim
+  alfa = 0
+
+  # finding the maximum number in the res tensor
+  for i in range(0, spatial_dim):
+    for j in range(0, spatial_dim):
+      for k in range(0, orient_dim):
+        if res[:, :, i, j][0][k] < 0:
+          sys.exit("Provided tensor cannot contain a negative number!")
+        max = res[:, :, i, j][0][k] if (res[:, :, i, j][0][k] > max) else max
+
+  max = float(max)
+
+  # creating the table
+  f, ax = plt.subplots(spatial_dim, spatial_dim, figsize=(10, 10), gridspec_kw = {'wspace':0, 'hspace':0})
+
+  if is_absolute_mode == 1:
+    f.suptitle(f"{title} bins abs histogram", fontsize=16)
+  else:
+    f.suptitle(f"{title} bins rel values", fontsize=16)
+
+  plt.subplots_adjust(wspace=0, hspace=0)
+  plt.rcParams['axes.linewidth'] = 5
+
+  # colorizing the table 
+  for i in range(spatial_dim):
+    for j in range(spatial_dim):
+      axx = ax
+      if spatial_dim != 1:
+        axx = ax[i][j]
+      axx.spines['bottom'].set_color('green')
+      axx.spines['top'].set_color('green')
+      axx.spines['right'].set_color('green')
+      axx.spines['left'].set_color('green')
+      axx.set_xticklabels([])
+      axx.set_yticklabels([])
+
+
+  # drawing the arrows corresponding the elements of the res tensor
+  for i in range(0, spatial_dim):
+    for j in range(0, spatial_dim):
+      for k in range(0, orient_dim):
+        if float(res[:, :, i, j][0][k]) == 0:
+          alfa += angle
+          continue
+        ratio = max / float(res[:, :, i, j][0][k])
+        if ratio <= 10 or is_absolute_mode == 1:
+          ratio = float(res[:, :, i, j][0][k]) if is_absolute_mode == 1 else float(res[:, :, i, j][0][k]) / max
+          axx = ax
+          if spatial_dim != 1:
+            axx = ax[i][j]
+          if alfa >= 0 and alfa <= 90:
+            axx.add_patch(mpatches.FancyArrowPatch((1, 1), (1 + ratio * math.cos(kornia.geometry.conversions.deg2rad(torch.tensor(alfa)).item()), 1 + ratio * math.sin(kornia.geometry.conversions.deg2rad(torch.tensor(alfa)).item())), mutation_scale=5))
+          elif alfa > 90 and alfa <= 180:
+            tmp = alfa
+            alfa = 180 - alfa
+            axx.add_patch(mpatches.FancyArrowPatch((1, 1), (1 - ratio * math.cos(kornia.geometry.conversions.deg2rad(torch.tensor(alfa)).item()), 1 + ratio * math.sin(kornia.geometry.conversions.deg2rad(torch.tensor(alfa)).item())), mutation_scale=5))
+            alfa = tmp         
+          elif alfa > 180 and alfa <= 270:
+            tmp = alfa
+            alfa -= 180
+            axx.add_patch(mpatches.FancyArrowPatch((1, 1), (1 - ratio * math.cos(kornia.geometry.conversions.deg2rad(torch.tensor(alfa)).item()), 1 - ratio * math.sin(kornia.geometry.conversions.deg2rad(torch.tensor(alfa)).item())), mutation_scale=5))
+            alfa = tmp          
+          else: 
+            tmp = alfa
+            alfa = 360 - alfa
+            axx.add_patch(mpatches.FancyArrowPatch((1, 1), (1 + ratio * math.cos(kornia.geometry.conversions.deg2rad(torch.tensor(alfa)).item()),  1 - ratio * math.sin(kornia.geometry.conversions.deg2rad(torch.tensor(alfa)).item())), mutation_scale=5))
+            alfa = tmp
+        alfa += angle     
+      axx.set(xlim=(0, 2), ylim=(0, 2))
+      alfa = 0
+  return f 
